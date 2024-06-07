65b34c2eaed3d13672794a92d740414a86ec1be3
Ihyafida <a710230038@student.ums.ac.id>
2024-06-07 20:18:28 +0700

    day3.py

---
+++ day3.py
@@ -0,0 +1,7 @@
+angka_1 =  input('masukkan angka 1 : ')
+angka_2 = input('masukkan angka 2 : ')
+
+hasil_bagi = float(angka_1) / float(angka_2)
+hasil_modulus = float(angka_1) / float(angka_2)
+print('hasil angka  1 dibagi angka 2 = ', hasil_bagi)
+print('hasil modulus  1 dibagi angka 2 = ', hasil_modulus)
