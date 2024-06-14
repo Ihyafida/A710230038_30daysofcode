1236f74efe5f644965eac48ca536d4ace5b89a63
ihyaalfidha <a710230038@student.ums.ac.id>
2024-06-14 13:29:11 +0700

    Delete day9.py

--- day9.py
+++
@@ -1,8 +0,0 @@
-phone_book = {}
-for i in range(5):
-    nama = input("masukkan nama teman ke-{}: ".format(i+1))
-    no_hp = input("masukkan nomor handphone teman ke-{}: ".format(i+1))
-    phone_book[nama] = no_hp
-print("phone book".center(60))
-for i, (nama, no_hp)in enumerate(phone_book.items(), 1):
-    print("{}. {} = {}".format(i, nama, no_hp))
