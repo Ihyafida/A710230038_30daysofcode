e56ab36ffc8ad9f860002b12d6191cf4eba5f54b
Ihyafida <a710230038@student.ums.ac.id>
2024-06-16 23:55:25 +0700

    day12.py

---
+++ day12.py
@@ -0,0 +1,11 @@
+def apakah_prima(bilangan):
+    if bilangan > 1:
+        for i in range(2, int(bilangan ** 0.5) + 1):
+            if (bilangan % i) == 0 :
+                return "bukan bilangan prima"
+        else :
+            return "bilangan prima"
+    else : return "bukan bilangan prima"
+bilangan = int(input("masukkan bilangan: "))
+hasil = apakah_prima(bilangan)
+print(hasil)
