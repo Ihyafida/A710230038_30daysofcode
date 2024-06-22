21812d59f6a7e279c965a34fb354f65e9f806b1f
Ihyafida <a710230038@student.ums.ac.id>
2024-06-22 18:11:16 +0700

    day17.py

---
+++ day17.py
@@ -0,0 +1,9 @@
+def sort(list):
+    for j in range (len(list)-1):
+        if lst[i]>lst[i+1]:
+            lst[i],lst[i+1]=lst[i+1]
+            print(">>", j, i, lst)
+
+list = [30, 23, 40, 11, 3]
+print('Angka yang akan diurutkan :', list)
+print('Proses sorting :') 
